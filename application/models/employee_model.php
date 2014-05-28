e99b61c7cf8140fa46f7d2f6c090a8c42e6a892a
saleh199 <saleh.saiid@gmail.com>
2014-05-28 19:58:51 +0300

    add password hashing

--- application/models/employee_model.php
+++ application/models/employee_model.php
@@ -5,10 +5,18 @@ class Employee_model extends MY_Model{
 	public $_table = "employee"; // Table name
 	public $primary_key = "employee_id"; // Table primary key
 
-	public $before_create = array( "timestamp" ); // observer before create row
+	public $before_create = array( "timestamp", "hashpassword" ); // observer before create row
+
+	private $salt = "*&^%$#@456789876543$%^&$#";
 
 	protected function timestamp($data){
 		$data["date_added"] = $data["date_modified"] = time();
+
+		return $data;
+	}
+
+	protected function hashpassword($data){
+		$data['password'] = md5($data["password"] . $this->salt . $data["password"]);
 
 		return $data;
 	}
