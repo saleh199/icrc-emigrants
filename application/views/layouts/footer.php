cf26a9cf37859a6b4c93b01826e85f716c013ba3
Saleh Saiid <saleh.saiid@gmail.com>
2014-06-08 15:03:34 +0300

    fix javascript files path

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,8 +1,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="<? echo $this->config->item('assets');?>js/jquery-1.11.1.min.js" type="text/JavaScript" language="javascript"></script>
+    <script src="<?php echo base_url('assets/js/jquery-1.11.1.min.js');?>" type="text/JavaScript" language="javascript"></script>
 
-    <script src="<? echo $this->config->item('assets');?>js/bootstrap.min.js" type="text/JavaScript" language="javascript"></script>
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js');?>" type="text/JavaScript" language="javascript"></script>
   </body>
 </html>
