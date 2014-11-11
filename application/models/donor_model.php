8fc2b240abd307d6480cefb57ab3ff814d328149
Saleh Saiid <saleh.saiid@gmail.com>
2014-11-11 12:07:05 +0200

    add trigger methods for date_added and date_modified in donor_model.php

--- application/models/donor_model.php
+++ application/models/donor_model.php
@@ -5,8 +5,25 @@ class donor_model extends MY_Model{
 	public $_table = "donor"; // Table name
 	public $primary_key = "donor_id"; // Table primary key
 
+	public $before_create = array( "timestamp" ); // observer before create row
+	public $before_update = array( "timestampUpdate" ); // observer before update row
+
+	protected $protected_attributes = array( "donor_id" );
+
 	public function __construct(){
 		parent::__construct();
+	}
+
+	protected function timestamp($data){
+		$data["date_added"] = $data["date_modified"] = time();
+
+		return $data;
+	}
+
+	protected function timestampUpdate($data){
+		$data["date_modified"] = time();
+
+		return $data;
 	}
 
 	public function dropdown($blank_text = ''){
