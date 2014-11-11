c538eb854a92b5f9ad2b74fbff5d956659e01539
Saleh Saiid <saleh.saiid@gmail.com>
2014-11-11 12:06:40 +0200

    add trigger methods for date_added and date_modified

--- application/models/material_model.php
+++ application/models/material_model.php
@@ -5,8 +5,25 @@ class material_model extends MY_Model{
 	public $_table = "material"; // Table name
 	public $primary_key = "material_id"; // Table primary key
 
+	public $before_create = array( "timestamp" ); // observer before create row
+	public $before_update = array( "timestampUpdate" ); // observer before update row
+
+	protected $protected_attributes = array( "material_id" );
+
 	public function __construct(){
 		parent::__construct();
+	}
+
+	protected function timestamp($data){
+		$data["date_added"] = $data["date_modified"] = time();
+
+		return $data;
+	}
+
+	protected function timestampUpdate($data){
+		$data["date_modified"] = time();
+
+		return $data;
 	}
 
 	public function dropdown($donor_id, $blank_text = ''){
