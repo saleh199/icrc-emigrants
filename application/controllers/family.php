064c1d8343b5aacdf9c9a2a2f445f51b88942f4d
Saleh Saiid <saleh.saiid@gmail.com>
2015-04-13 18:43:27 +0300

    edit charset

--- application/controllers/family.php
+++ application/controllers/family.php
@@ -571,7 +571,7 @@ class Family extends CI_Controller {
 		$data = $this->dbutil->csv_from_result($result, $delimiter, $newline);
 
 		// Sending headers to force the user to download the file
-		header('Content-Type: text/csv');
+		header('Content-Type: text/csv;charset=UTF-8');
 		header('Content-Disposition: attachment;filename="Dist_'.date('dMy').'.csv"');
 		header('Cache-Control: max-age=0');
 
